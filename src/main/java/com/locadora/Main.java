6870c22635af253ef8a646cfe12859efd3d79df7
Rharhuky <rharhuky@gmail.com>
2023-11-23 20:49:35 -0300

    add : classe Pessoa

---
+++ src/main/java/com/locadora/Main.java
@@ -0,0 +1,18 @@
+package com.locadora;
+
+import com.locadora.dao.VeiculoDAO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		VeiculoDAO dao = new VeiculoDAO("");
+//		dao.consultarTodosVeiculos();
+//		
+//		dao.deletarVeiculoByPelaPlaca("101010");
+//		dao.consultarTodosVeiculos();
+		
+		
+	}
+	
+}
